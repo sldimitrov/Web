e2ef9f0ce39cefeb75315ef96fb7d2e0d1b41e26
Slavi Dimitrov <slavi@orbisds.com>
2024-04-11 18:17:08 +0300

    re-write the functionality of my project on my own

--- ToDoApp/index.js
+++ ToDoApp/index.js
@@ -1,15 +1,15 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer');
+let addTaskButton = document.getElementById('addToDo');
+let toDoContainer = document.querySelector('.to-dos');
 let inputField = document.getElementById('inputField');
 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.classList.add('paragraph-styling')
-    paragraph.innerHTML = inputField.value;
+addTaskButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
-    inputField.value = "";
+    inputField.value = ""
     paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration="line-through";
+        paragraph.style.textDecoration = 'line-through';
     })
     paragraph.addEventListener('dblclick', function(){
         toDoContainer.removeChild(paragraph);
